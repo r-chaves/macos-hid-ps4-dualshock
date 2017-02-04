3c2fcf747be1414eb0b901d0f69eb8e016f32579
r-chaves <12619152+r-chaves@users.noreply.github.com>
2017-02-04 16:27:29 -0500

    start tracking list of gamepads

--- ps4-dualshock/ps4-dualshock/ViewController.m
+++ ps4-dualshock/ps4-dualshock/ViewController.m
@@ -10,24 +10,24 @@
 #import <IOKit/hid/IOHIDManager.h>
 
 @interface ViewController()
-{
-}
+@property NSMutableArray *ds4_list;
 @end
+
 
 static void handle_device_match
 (
-    void *          inContext,       // context from IOHIDManagerRegisterDeviceMatchingCallback
-    IOReturn        inResult,        // the result of the matching operation
-    void *          inSender,        // the IOHIDManagerRef for the new device
-    IOHIDDeviceRef  inIOHIDDeviceRef // the new HID device
+ void *          inContext,       // context from IOHIDManagerRegisterDeviceMatchingCallback
+ IOReturn        inResult,        // the result of the matching operation
+ void *          inSender,        // the IOHIDManagerRef for the new device
+ IOHIDDeviceRef  inIOHIDDeviceRef // the new HID device
 );
 
 static void handle_device_removal
 (
-    void *          inContext,       // context from IOHIDManagerRegisterDeviceMatchingCallback
-    IOReturn        inResult,        // the result of the removing operation
-    void *          inSender,        // the IOHIDManagerRef for the device being removed
-    IOHIDDeviceRef  inIOHIDDeviceRef // the removed HID device
+ void *          inContext,       // context from IOHIDManagerRegisterDeviceMatchingCallback
+ IOReturn        inResult,        // the result of the removing operation
+ void *          inSender,        // the IOHIDManagerRef for the device being removed
+ IOHIDDeviceRef  inIOHIDDeviceRef // the removed HID device
 );
 
 @implementation ViewController
@@ -38,31 +38,32 @@ static void handle_device_removal
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
     // Do any additional setup after loading the view.
-
+    
+    self.ds4_list = [[NSMutableArray alloc] initWithCapacity:0];
     
     hid_manager = IOHIDManagerCreate(
-        kCFAllocatorDefault, kIOHIDOptionsTypeNone);
-
+                                     kCFAllocatorDefault, kIOHIDOptionsTypeNone);
+    
     NSArray *matchingTypes =
-        @[  @{
-                @ kIOHIDDeviceUsagePageKey : @(kHIDPage_GenericDesktop),
-                @ kIOHIDDeviceUsageKey     : @(kHIDUsage_GD_Joystick)
-            },
-            @{
-                @ kIOHIDDeviceUsagePageKey : @(kHIDPage_GenericDesktop),
-                @ kIOHIDDeviceUsageKey     : @(kHIDUsage_GD_GamePad)
-            }
+    @[  @{
+            @ kIOHIDDeviceUsagePageKey : @(kHIDPage_GenericDesktop),
+              @ kIOHIDDeviceUsageKey     : @(kHIDUsage_GD_Joystick)
+              },
+        @{
+            @ kIOHIDDeviceUsagePageKey : @(kHIDPage_GenericDesktop),
+              @ kIOHIDDeviceUsageKey     : @(kHIDUsage_GD_GamePad)
+              }
         ];
     
     IOHIDManagerRegisterDeviceMatchingCallback(
-        hid_manager, handle_device_match, (__bridge void * _Nullable)(self));
+                                               hid_manager, handle_device_match, (__bridge void * _Nullable)(self));
     IOHIDManagerRegisterDeviceRemovalCallback(
-        hid_manager, handle_device_removal, (__bridge void * _Nullable)(self));
+                                              hid_manager, handle_device_removal, (__bridge void * _Nullable)(self));
     IOHIDManagerSetDeviceMatchingMultiple(hid_manager, (__bridge CFArrayRef)matchingTypes);
     IOHIDManagerScheduleWithRunLoop(
-        hid_manager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+                                    hid_manager, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
     IOHIDManagerOpen(hid_manager, kIOHIDOptionsTypeNone);
 }
 
@@ -70,32 +71,37 @@ static void handle_device_removal
 - (void)setRepresentedObject:(id)representedObject
 {
     [super setRepresentedObject:representedObject];
-
+    
     // Update the view, if already loaded.
 }
 
 static void handle_device_match
 (
-    void *          inContext,       // context from IOHIDManagerRegisterDeviceMatchingCallback
-    IOReturn        inResult,        // the result of the matching operation
-    void *          inSender,        // the IOHIDManagerRef for the new device
-    IOHIDDeviceRef  inIOHIDDeviceRef // the new HID device
+ void *          inContext,       // context from IOHIDManagerRegisterDeviceMatchingCallback
+ IOReturn        inResult,        // the result of the matching operation
+ void *          inSender,        // the IOHIDManagerRef for the new device
+ IOHIDDeviceRef  inIOHIDDeviceRef // the new HID device
 )
 {
     printf("%s(context: %p, result: %i, sender: %p, device: %p).\n",
            __PRETTY_FUNCTION__, inContext, inResult, inSender, inIOHIDDeviceRef);
+    ViewController *self = (__bridge ViewController *)inContext;
+    [self.ds4_list addObject:(__bridge id _Nonnull)(inIOHIDDeviceRef)];
 }
 
 static void handle_device_removal
 (
-    void *          inContext,       // context from IOHIDManagerRegisterDeviceMatchingCallback
-    IOReturn        inResult,        // the result of the removing operation
-    void *          inSender,        // the IOHIDManagerRef for the device being removed
-    IOHIDDeviceRef  inIOHIDDeviceRef // the removed HID device
+ void *          inContext,       // context from IOHIDManagerRegisterDeviceMatchingCallback
+ IOReturn        inResult,        // the result of the removing operation
+ void *          inSender,        // the IOHIDManagerRef for the device being removed
+ IOHIDDeviceRef  inIOHIDDeviceRef // the removed HID device
 )
 {
     printf("%s(context: %p, result: %i, sender: %p, device: %p).\n",
            __PRETTY_FUNCTION__, inContext, inResult, inSender, inIOHIDDeviceRef);
+    ViewController *self = (__bridge ViewController *)inContext;
+    
+    [self.ds4_list removeObject:(__bridge id _Nonnull)(inIOHIDDeviceRef)];
 }
 
 @end
